41f26b315693f4234ebd390595df3b8389bf1839
luozan <iluozan@126.com>
2024-08-05 17:12:00 +0800

    feat: 使用EBO

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -12,36 +12,27 @@ void Triangle::init()
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
     float vertices[] = {
-        -0.5f,
-        -0.5f,
-        0.0f,
-        //
-        0.5f,
-        -0.5f,
-        0.0f,
-        //
-        0.0f,
-        0.5f,
-        0.0f,
-        //
-        0.5f,
-        0.5f,
-        0.0f,
-        //
-        0.5f,
-        -0.5f,
-        0.0f,
-        //
-        0.0f,
-        0.5f,
-        0.0f,
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
+    };
+
+    unsigned int indices[] = {
+        // 注意索引从0开始!
+        // 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+        // 这样可以由下标代表顶点组合成矩形
+
+        0, 1, 3, // 第一个三角形
     };
 
     glGenBuffers(1, &VBO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     char *shader = R"(#version 330 core
 layout (location = 0) in vec3 aPos;
@@ -100,13 +91,13 @@ void main()
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
-    glBindVertexArray(VAO);
-
+    glBindVertexArray(0);
 }
 
 void Triangle::draw() const
 {
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
 }
