0faf85e609f15bbf7f6a76c4cb943767f5a4f88e
luozan <iluozan@126.com>
2024-08-21 16:02:55 +0800

    feat: 更新案例

---
+++ examples/demo004/src/main.cpp
@@ -0,0 +1,16 @@
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+#include <iostream>
+#include <rendering_engine/application.h>
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+    Application *app = new Application(SCR_WIDTH, SCR_HEIGHT, const_cast<char *>("LearningOpenGL"));
+    app->run();
+    return 0;
+}
